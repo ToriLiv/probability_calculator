2d36b02b5f5154f6dc0546eeab494335914c340a
ToriLiv <oamossquintanilla@gmail.com>
2025-10-24 09:50:26 -0600

    Fix: Respuesta

--- script.js
+++ script.js
@@ -85,6 +85,6 @@ submitBtn.addEventListener("click", (e) => {
   //--------------mostrar resultado en pantalla----------------
   Swal.fire({
     title: "Resultado",
-    text: `P(X=${x}) = ${result.toFixed(10)}% = ${(result * 100).toFixed(6)}%`,
+    text: `P(X=${x}) = ${(result * 100).toFixed(4)}%`,
   });
 });
